a96b165cb69935cea8becc19664f05a04c917e4e
David Perelman <dperelman3@gmail.com>
2024-03-14 18:13:50 +0200

    create gui elements

--- gui.py
+++ gui.py
@@ -1,6 +1,12 @@
 import tkinter
 import customtkinter
 
+from main import start_run
+
+def submit():
+    start_run(video_url.get(), album_name.get(), artist_name.get(), recording_date.get())
+    # label.configure(text=my_entry.get())
+
 # System Setting
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
@@ -11,13 +17,40 @@ app.geometry('720x480')
 app.title('YouTube Splitter')
 
 # Adding UI Elements
-title = customtkinter.CTkLabel(app, text='Insert a youtube link')
-title.pack(padx=10, pady=10)
+label = customtkinter.CTkLabel(app, text='Enter youtube link')
+label.pack(padx=10, pady=10)
 
 # Link input 
-url_var = tkinter.StringVar()
-link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
-link.pack()
+video_url = customtkinter.CTkEntry(app, width=350, height=40)
+video_url.pack(padx=10)
+
+# Adding UI Elements
+label = customtkinter.CTkLabel(app, text='Enter album name')
+label.pack(padx=10, pady=10)
+
+# Album input 
+album_name = customtkinter.CTkEntry(app, width=350, height=40)
+album_name.pack(padx=10)
+
+# Adding UI Elements
+label = customtkinter.CTkLabel(app, text='Enter artist name')
+label.pack(padx=10, pady=10)
+
+# Album input 
+artist_name = customtkinter.CTkEntry(app, width=350, height=40)
+artist_name.pack(padx=10)
+
+# Adding UI Elements
+label = customtkinter.CTkLabel(app, text='Enter recording year')
+label.pack(padx=10, pady=10)
+
+# Album input 
+recording_date = customtkinter.CTkEntry(app, width=350, height=40)
+recording_date.pack(padx=10)
+
+# Download button
+button = customtkinter.CTkButton(app, text='Download', command=submit, width=350, height=40)
+button.pack(padx=20, pady=20)
 
 # Run app
 app.mainloop()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from tkinter import Entry
 from download_video import startDownload
 from utils import create_audio, create_chapters_data, get_video_length, read_text, split_audio_files, get_desc
 # from langdetect import detect
@@ -19,78 +20,161 @@ software_path = os.getcwd()
 # Arctic Monkeys - Glastonbury 2007 Live - Full Show Remastered HD
 # video_url = 'https://www.youtube.com/watch?v=36GNdaxlA0k'
 
-try:
-    video_url = input('Enter youtube link: ')
-    album_name = input('Enter album name: ').title()
-    artist_name = input('Enter artist: ').title()
-    recording_date = input('Enter recording year: ')
+# try:
+#     video_url = input('Enter youtube link: ')
+#     album_name = input('Enter album name: ').title()
+#     artist_name = input('Enter artist: ').title()
+#     recording_date = input('Enter recording year: ')
 
-    # album_name_lang_check = detect(album_name)
-    # artist_name_lang_check = detect(artist_name)
+#     # album_name_lang_check = detect(album_name)
+#     # artist_name_lang_check = detect(artist_name)
 
-    # if album_name_lang_check != 'he':
-    #     album_name.title()
+#     # if album_name_lang_check != 'he':
+#     #     album_name.title()
 
-    # if artist_name_lang_check != 'he':
-    #     artist_name.title()
+#     # if artist_name_lang_check != 'he':
+#     #     artist_name.title()
 
-    print("\nDownloading...")
+#     print("\nDownloading...")
 
-    # # # Download video
-    video_path = startDownload(video_url)
+#     # # # Download video
+#     video_path = startDownload(video_url)
 
-    # # # Get video length
-    video_length = get_video_length(video_path)
+#     # # # Get video length
+#     video_length = get_video_length(video_path)
 
-    # # # Get video name
-    video_name = os.path.basename(video_path[:-4])
+#     # # # Get video name
+#     video_name = os.path.basename(video_path[:-4])
 
-    # # # Get text folder
-    text_dir = os.listdir('./chapters_files') 
+#     # # # Get text folder
+#     text_dir = os.listdir('./chapters_files') 
 
-    # # # Get video description
-    description = get_desc(video_url)
+#     # # # Get video description
+#     description = get_desc(video_url)
     
-    if len(description) > 0:
-        # # # Get video chapters from description
-        chapters = create_chapters_data(description, video_length, 'description')
-        # # # Checking if the video chapters from description is empty or not 
-        if len(chapters['start'] or chapters['end'] or chapters['title']) > 0:
-            done = split_audio_files(artist_name, album_name, recording_date, chapters, video_path)
+#     if len(description) > 0:
+#         # # # Get video chapters from description
+#         chapters = create_chapters_data(description, video_length, 'description')
+#         # # # Checking if the video chapters from description is empty or not 
+#         if len(chapters['start'] or chapters['end'] or chapters['title']) > 0:
+#             done = split_audio_files(artist_name, album_name, recording_date, chapters, video_path)
+#         # # # Checking if the text directory is empty or not 
+#         elif len(text_dir) > 0: 
+#         # # # Checking if the text chapters belongs to the video 
+#             if f'{video_name}.txt' in text_dir:
+#                 text_path = r'{}\chapters_files\{}.txt'.format(software_path, video_name)
+
+#                 text = read_text(text_path)
+                    
+#                 text_content = r""" {} """.format(text)
+
+#                 chapters_data = create_chapters_data(text_content, video_length, 'text_file')
+
+#                 done = split_audio_files(artist_name, album_name, recording_date, chapters_data, video_path)
+#             else: 
+#             # # # Create single audio file
+#                 create_audio(artist_name, album_name, recording_date, video_path) 
+#     # # # Checking if the text directory is empty or not 
+#     elif len(text_dir) > 0: 
+#         # # # Checking if the text chapters belongs to the video 
+#         if f'{video_name}.txt' in text_dir:
+#             text_path = r'{}\chapters_files\{}.txt'.format(software_path, video_name)
+
+#             text = read_text(text_path)
+                        
+#             text_content = r""" {} """.format(text)
+
+#             chapters_data = create_chapters_data(text_content, video_length, 'text_file')
+            
+#             done = split_audio_files(artist_name, album_name, recording_date, chapters_data, video_path)
+#         else: 
+#             # # # Create single audio file
+#             create_audio(artist_name, album_name, recording_date, video_path) 
+
+#     print('Done!')
+# except OSError:
+#     # Ignore OSError
+#     pass
+
+def start_run(video_url, album_name, artist_name, recording_date):
+    print('Hello from main')
+    print(video_url)
+    print(album_name)
+    print(artist_name)
+    print(recording_date)
+    return
+    try:
+        video_url = input('Enter youtube link: ')
+        album_name = input('Enter album name: ').title()
+        artist_name = input('Enter artist: ').title()
+        recording_date = input('Enter recording year: ')
+
+        # album_name_lang_check = detect(album_name)
+        # artist_name_lang_check = detect(artist_name)
+
+        # if album_name_lang_check != 'he':
+        #     album_name.title()
+
+        # if artist_name_lang_check != 'he':
+        #     artist_name.title()
+
+        print("\nDownloading...")
+
+        # # # Download video
+        video_path = startDownload(video_url)
+
+        # # # Get video length
+        video_length = get_video_length(video_path)
+
+        # # # Get video name
+        video_name = os.path.basename(video_path[:-4])
+
+        # # # Get text folder
+        text_dir = os.listdir('./chapters_files') 
+
+        # # # Get video description
+        description = get_desc(video_url)
+        
+        if len(description) > 0:
+            # # # Get video chapters from description
+            chapters = create_chapters_data(description, video_length, 'description')
+            # # # Checking if the video chapters from description is empty or not 
+            if len(chapters['start'] or chapters['end'] or chapters['title']) > 0:
+                done = split_audio_files(artist_name, album_name, recording_date, chapters, video_path)
+            # # # Checking if the text directory is empty or not 
+            elif len(text_dir) > 0: 
+            # # # Checking if the text chapters belongs to the video 
+                if f'{video_name}.txt' in text_dir:
+                    text_path = r'{}\chapters_files\{}.txt'.format(software_path, video_name)
+
+                    text = read_text(text_path)
+                        
+                    text_content = r""" {} """.format(text)
+
+                    chapters_data = create_chapters_data(text_content, video_length, 'text_file')
+
+                    done = split_audio_files(artist_name, album_name, recording_date, chapters_data, video_path)
+                else: 
+                # # # Create single audio file
+                    create_audio(artist_name, album_name, recording_date, video_path) 
         # # # Checking if the text directory is empty or not 
         elif len(text_dir) > 0: 
-        # # # Checking if the text chapters belongs to the video 
+            # # # Checking if the text chapters belongs to the video 
             if f'{video_name}.txt' in text_dir:
                 text_path = r'{}\chapters_files\{}.txt'.format(software_path, video_name)
 
                 text = read_text(text_path)
-                    
+                            
                 text_content = r""" {} """.format(text)
 
                 chapters_data = create_chapters_data(text_content, video_length, 'text_file')
-
+                
                 done = split_audio_files(artist_name, album_name, recording_date, chapters_data, video_path)
             else: 
-            # # # Create single audio file
+                # # # Create single audio file
                 create_audio(artist_name, album_name, recording_date, video_path) 
-    # # # Checking if the text directory is empty or not 
-    elif len(text_dir) > 0: 
-        # # # Checking if the text chapters belongs to the video 
-        if f'{video_name}.txt' in text_dir:
-            text_path = r'{}\chapters_files\{}.txt'.format(software_path, video_name)
 
-            text = read_text(text_path)
-                        
-            text_content = r""" {} """.format(text)
-
-            chapters_data = create_chapters_data(text_content, video_length, 'text_file')
-            
-            done = split_audio_files(artist_name, album_name, recording_date, chapters_data, video_path)
-        else: 
-            # # # Create single audio file
-            create_audio(artist_name, album_name, recording_date, video_path) 
-
-    print('Done!')
-except OSError:
-    # Ignore OSError
-    pass
+        print('Done!')
+    except OSError:
+        # Ignore OSError
+        pass

87ba24c8239b9fc6f909a202925b583199423241
David Perelman <dperelman3@gmail.com>
2024-03-15 02:43:19 +0200

    yt_api.py: get_video_length & get_video_comments. yt_api_utils.py: extract_video_duration & extract_video_timestamp_comments

---
+++ yt_api.py
@@ -0,0 +1,80 @@
+import json
+import googleapiclient.discovery
+
+from yt_api_utils import extract_video_duration, extract_video_timestamp_comments
+
+api_key = 'AIzaSyA3BF_ZeABmwhs3APurFXccAoeZncKu0SI'
+
+# Create a YouTube Data API client
+youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
+video_id='36GNdaxlA0k'
+
+def get_fake_video_length():
+    # Opening JSON file
+    f = open('contentDetails.json')
+    # returns JSON object as a dictionary
+    video_response_json = json.load(f)
+    # Closing file
+    f.close()
+
+    return video_response_json
+
+def get_fake_video_comments():
+    # Opening JSON file
+    f = open('snippet.json')
+    # returns JSON object as a dictionary
+    response_json = json.load(f)
+    # Closing file
+    f.close()
+
+    comments_response = []
+
+    for item in response_json['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments_response.append(comment)
+
+    return comments_response
+
+# def get_video_length():
+#     # # Define parameters for the API request
+#     video_response = youtube.videos().list(
+#         part='snippet,contentDetails',
+#         id=video_id
+#     ).execute()
+
+#     return video_response
+    
+# def get_video_comments():
+#     # Call the API to retrieve comments for the specified video
+#     comments = []
+#     next_page_token = None
+
+#     while True:
+#         response = youtube.commentThreads().list(
+#             part="snippet",
+#             videoId=video_id,
+#             maxResults=100,
+#             pageToken=next_page_token
+#         ).execute()
+
+#         # Process each comment in the response
+#         for item in response['items']:
+#             comment = item['snippet']['topLevelComment']['snippet']
+#             comments.append(comment)
+
+#         # Check if there are more pages of comments
+#         next_page_token = response.get('nextPageToken')
+#         if not next_page_token:
+#             break
+
+#     return comments
+
+# # Extract duration from response:
+video_response = get_fake_video_length()
+video_length = extract_video_duration(video_response)
+
+# Extract timestamps from response:
+comments_response = get_fake_video_comments()
+video_timestamps = extract_video_timestamp_comments(comments_response, video_length)
+
+print(video_timestamps)
---
+++ yt_api_utils.py
@@ -0,0 +1,67 @@
+import re
+
+def extract_video_duration(video_response):
+    # Extract duration from response
+    duration = video_response['items'][0]['contentDetails']['duration']
+
+    # Parse the duration using regular expression
+    match = re.match(r'PT(\d+H)?(\d+M)?(\d+S)?', duration)
+    hours = int(match.group(1)[:-1]) if match.group(1) else 0
+    minutes = int(match.group(2)[:-1]) if match.group(2) else 0
+    seconds = int(match.group(3)[:-1]) if match.group(3) else 0
+
+    # Calculate total length in seconds
+    video_length = f"0{hours}:{minutes}:{seconds}"
+
+    return video_length
+
+def extract_video_timestamp_comments(comments, video_length):
+    html_text = ''
+    # Regular expression pattern to match chapter timestamps
+    pattern = r'(?<!href=\")(?:\d+:)?\d+:\d+'  # Matches patterns like '0:00', '03:28', etc.
+
+    # Loop through the comments and check for chapter timestamps
+    for comment in comments:
+        text = comment['textDisplay']
+        
+        # Use regular expression to find chapter timestamps in the comment text
+        matches = re.findall(pattern, text)
+        
+        # If timestamps are found, print the comment and timestamps
+        if matches:
+            if len(text) > 100:
+                html_text = text
+
+    # Regular expression pattern to match the titles between <a> and <br> tags
+    pattern = r'<a[^>]*>(.*?)<\/a>(?:<\/a>)?\s*([^<]*)(?:<br>|\s*\Z)'
+
+    # Use regular expression to find titles in the HTML text
+    titles = re.findall(pattern, html_text)
+
+    data = [{
+        "titles": [],
+        "timestamps_start": [],
+        "timestamps_end": [],
+    }]
+
+    # Print the extracted titles
+    for time, title in titles:
+        if len(time) == 4:
+                time = f"00:0{time.strip()}"
+                data[0]['timestamps_start'].append(time)
+        if len(time) == 5:
+                time = f"00:{time.strip()}"
+                data[0]['timestamps_start'].append(time)
+        if len(time) == 7:
+                time = f"0{time.strip()}"
+                data[0]['timestamps_start'].append(time)
+        data[0]['titles'].append(title.strip())
+    
+    for i in range(len(data[0]['timestamps_start'])):
+        if i < len(data[0]['timestamps_start']) - 1:
+            data[0]['timestamps_end'].append(data[0]['timestamps_start'][i + 1])
+
+    data[0]['timestamps_start'][0] = "00:00:00"
+    data[0]['timestamps_end'][-1] = video_length
+
+    return data
